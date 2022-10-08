137e6fd8b126cf6ad106bd9a1cc949cccca9c4c0
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2022-10-07 21:38:57 -0300

    add some rust sheel commands
    - based on this post https://zaiste.net/posts/shell-commands-rust/

--- resources/config.fish
+++ resources/config.fish
@@ -1,4 +1,10 @@
 if status --is-login
+    # add cargo bin to path
     set -gx PATH $PATH ~/.cargo/bin
 end
+
+# init starship on fish
 starship init fish | source
+
+# install asdf
+source ~/.asdf/asdf.fish
